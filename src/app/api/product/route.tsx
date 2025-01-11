62538c30b5f60b186de098e140439a1101043246
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-11 15:37:19 +0500

    post method

--- src/app/api/product/route.tsx
+++ src/app/api/product/route.tsx
@@ -3,7 +3,10 @@ import { productLists } from '../../../../libs/db'
 import React from 'react'
 
 export const GET = async () => {
-   
     return NextResponse.json({ data: productLists }, { status: 201 })
+}
 
+
+export const POST = async () => {
+    return NextResponse.json({ data: productLists }, { status: 201 })
 }
