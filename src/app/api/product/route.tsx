b025fcb0cde22d84cb407c1dc1938c16fc18338a
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-11 15:44:50 +0500

    data

--- src/app/api/product/route.tsx
+++ src/app/api/product/route.tsx
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server'
 import { productLists } from '../../../../libs/db'
 import React from 'react'
+import { log } from 'node:console'
 
 export const GET = async () => {
     return NextResponse.json({ data: productLists }, { status: 201 })
 }
 
-export const POST = async (request:Request) => {
+export const POST = async (request: Request) => {
+    const data = await request.json()
+    console.log(data);
     return NextResponse.json({ data: productLists }, { status: 200 })
 }
