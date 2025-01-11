046ea51a235fdc318955da312171852020f2e2ef
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-11 18:59:22 +0500

    completed code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const getAllProducts = async () => {
-  const response = await fetch("http://localhost:3000/api/product")
-  if (!response.ok)
-    throw new Error("API NOT FOUND ")
-  const result = await response.json()
-  return await result
-}
+  const response = await fetch("http://localhost:3000/api/product");
+  if (!response.ok) throw new Error("API NOT FOUND");
+  const result = await response.json();
+  return result;
+};
 
-// const page  = () => {
-  export default async function Home(){
-  const product = await getAllProducts()
+export default async function Home() {
+  const product = await getAllProducts();
   console.log(product);
+
   return (
     <div>
-      NEXTJS API ROUTES
+      <h1>NEXTJS API ROUTES</h1>
+      {product?.data?.map((prod: any) => {
+        return (
+          <div key={prod?.id}>
+            <h2>Product: {prod?.title}</h2>
+            <p>Description: {prod?.description}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
-
