b547a812dbc14f2a9e8c8cbda1b1e323b749586a
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-11 18:43:57 +0500

    result

--- src/app/api/product/route.tsx
+++ src/app/api/product/route.tsx
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server'
 import { productLists } from '../../../../libs/db'
-import React from 'react'
-import { log } from 'node:console'
 
 export const GET = async () => {
     return NextResponse.json({ data: productLists }, { status: 201 })
@@ -12,3 +10,10 @@ export const POST = async (request: Request) => {
     console.log(data);
     return NextResponse.json({ data: productLists }, { status: 200 })
 }
+
+
+// export const PATCH = async (request: Request) => {
+//     const data = await request.json()
+//     console.log(data);
+//     return NextResponse.json({ data: productLists }, { status: 200 })
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
+const getAllProducts = async () => {
+  const response = await fetch("http://localhost:3000/api/product")
+  if (!response.ok)
+    throw new Error("API NOT FOUND ")
+  const result = await response.json()
+  return result
+}
+
 const page = () => {
+  const product = getAllProducts()
+  console.log(product);
+
   return (
     <div>
       NEXTJS API ROUTES
